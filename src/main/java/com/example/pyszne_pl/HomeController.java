58341a9f585c57b17ce353c6d983067d1dede766
Marcinek <marcinbator.ofc@gmail.com>
2023-06-05 21:47:09 +0200

    init - learning

---
+++ src/main/java/com/example/pyszne_pl/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.pyszne_pl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
