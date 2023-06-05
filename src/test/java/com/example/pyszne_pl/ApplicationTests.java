d2abc64646c1cdf451117353332a5222ac49d7a2
Marcinek <marcinbator.ofc@gmail.com>
2023-06-05 21:07:08 +0200

    init

---
+++ src/test/java/com/example/pyszne_pl/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pyszne_pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
